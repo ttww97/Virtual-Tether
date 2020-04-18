f29b109d1043f27b5a9b1e14e719b310c5f72e3a
Stephen Waring <u5182264@anu.edu.au>
2020-04-18 11:13:14 +1000

    made constat arrays for fellows oval

--- test/pathtest.js
+++
@@ -1,5 +0,0 @@
-var innerlatDir = "paths\fellows\innerlat.txt";
-var innerlongDir = "paths\fellows\innerlong.txt";
-var fs = require('fs');
-var contents = fs.readFileSync(innerlatDir, 'utf8');
-console.log(contents);
--- test/pathtest.ts
+++ test/pathtest.ts
@@ -1,7 +1,1 @@
 
-
-var innerlatDir :string = "paths\fellows\innerlat.txt"
-var innerlongDir : string = "paths\fellows\innerlong.txt"
-var fs = require('fs');
-var contents = fs.readFileSync(innerlatDir, 'utf8');
-console.log(contents);
