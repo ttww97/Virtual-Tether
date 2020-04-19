a013b71b4fcaa3c225f0d104cfa5e858c7648ff5
Ruichi Wu <wurc199733@gmail.com>
2020-04-19 12:13:23 +1000

    complete the accessibility part

--- components/pages/MainWelcome.tsx
+++ components/pages/MainWelcome.tsx
@@ -2,52 +2,110 @@ import React from "react";
 import {
   StyleSheet,
   View,
-  Button,
-  Alert,
   Text,
   Image,
-  Dimensions
+  Button,
+  Alert,
+  TouchableOpacity
 } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
-import DropdownMenu from "react-native-dropdown-menu";
 
 const MainWelcome = ({ navigation }) => {
-  var pathData = [["Fellow Oval", "Green Oval"]];
-  const screenWidth = Math.round(Dimensions.get("window").width);
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text style={styles.headerText}>Virtual Tether</Text>
-        <Icon name="bars" size={30} color="#fff" />
-      </View>
-
-      <View style={styles.banner}>
-        <Icon name="rocket" size={30} color="#fff" style={styles.bannerIcon} />
-        <View style={styles.bannerImgContainer}>
-          <Image
-            source={require("../../assets/selfie.jpg")}
-            style={styles.selfie}
-          />
-        </View>
-        <Text style={styles.name}>Dave Fluid</Text>
-        <Text style={styles.address}>San Francisco, CA</Text>
-        <Text style={styles.emergency}>Emergency Contact: John Smith</Text>
-        <Text style={styles.emergency}>Emergency Number: 0123456</Text>
-      </View>
-
-      <View style={styles.select}>
-        <Text style={styles.selectPath}>Select Path</Text>
-        <DropdownMenu
-          data={pathData}
-          bgColor="#c0c0c0"
-          titleStyle={{ fontSize: 20, color: "#000000" }}
-          optionTextStyle={{ fontSize: 18, color: "#000000" }}
+        <Text style={styles.headerText} accessibilityLabel="Virtual Tether">
+          Virtual Tether
+        </Text>
+        <Icon
+          name="bars"
+          size={30}
+          color="#fff"
+          accessibilityLabel="menu bar button"
         />
       </View>
 
-      <View style={styles.twoButtons}>
-        <View styles={styles.buttonOne}></View>
-        <View styles={styles.buttonTwo}></View>
+      <View style={styles.banner}>
+        <Icon
+          name="edit"
+          size={30}
+          color="#6CB4EE"
+          style={styles.bannerIcon}
+          accessibilityLabel="edit personal information button"
+        />
+        <View style={styles.bannerImgContainer}>
+          <Image
+            accessible={true}
+            source={require("../../assets/selfie.jpg")}
+            style={styles.selfie}
+            accessibilityLabel="profile picture"
+          />
+        </View>
+        <Text style={styles.name} accessibilityLabel="name Dave Fluid">
+          Dave Fluid
+        </Text>
+        <Text
+          style={styles.address}
+          accessibilityLabel="address San Francisco, CA"
+        >
+          San Francisco, CA
+        </Text>
+        <Text
+          style={styles.emergency}
+          accessibilityLabel="Emergency Contact: John Smith"
+        >
+          Emergency Contact: John Smith
+        </Text>
+        <Text
+          style={styles.emergency}
+          accessibilityLabel="Emergency Number: 0123456"
+        >
+          Emergency Number: 0123456
+        </Text>
+      </View>
+
+      <View style={styles.select}>
+        <TouchableOpacity
+          accessibilityLabel="select path button"
+          onPress={() => {
+            Alert.alert("Simple Button pressed");
+          }}
+        >
+          <View
+            style={{
+              backgroundColor: "//#endregion6CB4EE",
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: 15,
+              height: 100
+            }}
+          >
+            <Text style={{ color: "white", fontSize: 30, height: 30 }}>
+              SELECT PATH
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.startPath}>
+        <Text style={styles.startPathText} accessibilityLabel="start path">
+          Start Path
+        </Text>
+        <TouchableOpacity
+          onPress={() => {
+            Alert.alert("Simple Button pressed");
+          }}
+        >
+          <View>
+            <Icon
+              name="arrow-circle-right"
+              size={30}
+              color="#fff"
+              style={styles.startPathButton}
+              accessibilityLabel="start path button"
+            />
+          </View>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -63,15 +121,12 @@ const styles = StyleSheet.create({
   },
 
   header: {
-    flex: 1,
+    flex: 0.1,
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    height: 60,
     width: "100%",
     backgroundColor: "#C0C0C0",
-    position: "absolute",
-    top: 0,
     paddingLeft: 15,
     paddingRight: 15
   },
@@ -83,73 +138,86 @@ const styles = StyleSheet.create({
   },
 
   banner: {
+    flex: 0.5,
+    flexDirection: "column",
+    justifyContent: "flex-start",
+
     width: "100%",
-    height: 350,
-    position: "absolute",
-    top: 60,
     borderBottomWidth: 1,
     borderBottomColor: "#C0C0C0",
     borderBottomStartRadius: 50,
-    borderBottomEndRadius: 50,
-    backgroundColor: "green"
+    borderBottomEndRadius: 50
   },
 
   bannerIcon: {
     textAlign: "right",
+    fontSize: 40,
     marginRight: 15,
-    marginTop: 15,
-    marginBottom: 15
+    marginTop: 10
   },
+
   bannerImgContainer: {
-    flex: 0.6,
+    flex: 0.8,
+    marginBottom: 20,
     justifyContent: "center",
     alignItems: "center"
   },
+
   selfie: {
     width: 100,
     height: 100,
     borderRadius: 50
   },
+
   name: {
     textAlign: "center",
     fontSize: 30,
     fontWeight: "500",
-    marginBottom: 10,
+    marginBottom: 20,
     marginTop: 5
   },
+
   address: {
     textAlign: "center",
     fontSize: 25,
     fontWeight: "400",
-    marginBottom: 5
+    marginBottom: 20
   },
+
   emergency: {
     textAlign: "center",
     fontSize: 22,
-    fontWeight: "400"
+    fontWeight: "400",
+    marginBottom: 20
   },
 
   select: {
-    backgroundColor: "red",
     width: "100%",
-    height: 150,
-    position: "absolute",
-    top: 410,
+    flex: 0.2,
+    textAlign: "center",
+    justifyContent: "center",
     paddingHorizontal: 40,
     paddingVertical: 10
   },
-  selectPath: {
-    fontSize: 25,
-    marginBottom: 10
+
+  startPath: {
+    flex: 0.3,
+    width: "100%",
+    textAlign: "center",
+    justifyContent: "center"
   },
-  twoButtons: {
-    position: "absolute",
-    top: 560,
-    backgroundColor: "yellow",
-    width: "100%"
+
+  startPathText: {
+    textAlign: "center",
+    fontSize: 30,
+    fontWeight: "400"
   },
-  buttonOne: {},
-  buttonTwo: {}
+
+  startPathButton: {
+    textAlign: "center",
+    fontSize: 150,
+    color: "red"
+  }
 });
 
 export default MainWelcome;
