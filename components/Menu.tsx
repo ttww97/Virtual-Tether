fbf854d4ce39e52af34300ef36c66a00d111ac3c
Aidan <aidansawers_1@hotnmail.com>
2020-04-24 12:33:25 +1000

    Merged conflicts

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,9 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-import { StyleSheet, Text, View, Button } from "react-native";
-
-const Menu = ({ navigation }) => {
-=======
 import { StyleSheet, Text, View, Button, Alert } from "react-native";
 
 const Menu = ({ navigation }) => {
@@ -16,7 +11,6 @@ const Menu = ({ navigation }) => {
     );
   };
 
->>>>>>> 29446a515946eaa5149358c510090fe3763e1b5b
   return (
     <View style={styles.container}>
       <Text
@@ -30,11 +24,8 @@ const Menu = ({ navigation }) => {
           accessibilityLabel="Enter the application"
           title="Enter"
           onPress={() => {
-<<<<<<< HEAD
             navigation.navigate("Welcome - page");
-=======
             navigation.navigate("Nav - page");
->>>>>>> 29446a515946eaa5149358c510090fe3763e1b5b
           }}
         ></Button>
         <Button
