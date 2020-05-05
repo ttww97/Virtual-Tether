b5483999c4273ed214753a9a01372ce298c06db7
Ruichi Wu <wurc199733@gmail.com>
2020-05-05 12:25:24 +1000

    added useEffect

--- util/outputAudios.ts
+++ util/outputAudios.ts
@@ -1,4 +1,5 @@
 import { shallowEqual, useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
 import { audios } from "./audioInstances";
 import { Audio } from "expo-av";
 
@@ -10,12 +11,15 @@ export const output = () => {
     state => state.communication.constantValue,
     shallowEqual
   );
-  // if the algorithmMessage is in the range1, play the audio1
-  for (let i = 0; i < audios.length; i++) {
-    if (audios[i].range.isInRange(algorithmMessage)) {
-      //const audioFile = require(`../assets/audios/${audios[i].output}.mp3`);
-      const statusPalay = { shouldPlay: true };
-      playbackObject = Audio.Sound.createAsync(audios[i].output, statusPalay);
+
+  useEffect(() => {
+    console.log("Constant value changed to " + algorithmMessage);
+    for (let i = 0; i < audios.length; i++) {
+      if (audios[i].range.isInRange(algorithmMessage)) {
+        //const audioFile = require(`../assets/audios/${audios[i].output}.mp3`);
+        const statusPalay = { shouldPlay: true };
+        playbackObject = Audio.Sound.createAsync(audios[i].output, statusPalay);
+      }
     }
-  }
+  }, [algorithmMessage]);
 };
